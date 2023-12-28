24964842596811023764b747535182cfbc3e9680
genie <sengercool@gmail.com>
2023-12-28 18:35:13 +0800

    Cprogramlearn

---
+++ HI.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    /* 我的第一个 C 程序 */
+    printf("Hello, World! \n");
+    printf("Hi, World! \n");
+    printf("\n"); 
+    system("pause"); 
+    return 0;
+}
